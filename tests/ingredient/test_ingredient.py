5c4139997016f2b34c1940530dd3e5cac6958b3b
Felipe Duarte <feduarte.dev@gmail.com>
2024-03-14 14:08:08 -0300

    requisito 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,27 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import (
+    Ingredient,
+    Restriction,
+)  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredientA = Ingredient("salmão")
+    ingredientB = Ingredient("salmão")
+    ingredientC = Ingredient("farinha")
+
+    assert ingredientA == ingredientB
+    assert ingredientA != ingredientC
+
+    assert hash(ingredientA) == hash(ingredientB)
+    assert hash(ingredientB) != hash(ingredientC)
+
+    assert ingredientA.name == "salmão"
+
+    assert repr(ingredientA) == "Ingredient('salmão')"
+
+    assert ingredientA.restrictions == {
+        Restriction.SEAFOOD,
+        Restriction.ANIMAL_DERIVED,
+        Restriction.ANIMAL_MEAT,
+    }
