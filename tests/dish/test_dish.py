5ca1df81b2ed1ba1bca45b9e006c0f2a39e56815
Felipe Duarte <feduarte.dev@gmail.com>
2024-03-14 15:07:44 -0300

    requisito 2

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,6 +1,32 @@
 from src.models.dish import Dish  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient
+import pytest
 
 
 # Req 2
 def test_dish():
-    pass
+    recipeA = Dish("Recipe A", 10.99)
+    recipeB = Dish("Recipe A", 10.99)
+    recipeC = Dish("Recipe C", 12.99)
+    assert recipeA.name == "Recipe A"
+
+    ingredientA = Ingredient("Ingredient A")
+    recipeA.add_ingredient_dependency(ingredientA, 3)
+    assert recipeA.get_ingredients() == {Ingredient("Ingredient A")}
+
+    assert recipeA == recipeB
+
+    assert recipeA.get_restrictions() == set()
+
+    assert hash(recipeA) == hash(recipeB)
+    assert hash(recipeA) != hash(recipeC)
+
+    assert repr(recipeA) == "Dish('Recipe A', R$10.99)"
+
+    with pytest.raises(TypeError, match="Dish price must be float."):
+        Dish("Recipe D", "10.00")
+
+    with pytest.raises(
+        ValueError, match="Dish price must be greater then zero."
+    ):
+        Dish("Recipe E", -10.00)
